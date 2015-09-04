8c73ac2cebf26dc30c90089deebd9f06e6b87a6c
Thomas Bytheway <tb403@cam.ac.uk>
2015-09-04 02:01:24 +0100

    Test catching is_kernel_module_active not implemented

--- escpos/printer.py
+++ escpos/printer.py
@@ -40,11 +40,14 @@ class Usb(Escpos):
         if self.device is None:
             print "Cable isn't plugged in"
 
-        if self.device.is_kernel_driver_active(0):
-            try:
-                self.device.detach_kernel_driver(0)
-            except usb.core.USBError as e:
-                print "Could not detatch kernel driver: %s" % str(e)
+        try:
+            if self.device.is_kernel_driver_active(0):
+                try:
+                    self.device.detach_kernel_driver(0)
+                except usb.core.USBError as e:
+                    print "Could not detatch kernel driver: %s" % str(e)
+        except NotImplementedError:
+            print "Missing kernel driver check, skipping."
 
         try:
             self.device.set_configuration()
